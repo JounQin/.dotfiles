2bd4ca9938294bac19631244baf0226862f06eb3
JounQin <admin@1stg.me>
2019-07-14 19:32:06 +0800

    :wrench:try again to fix CI errors :)

--- setup.sh
+++ setup.sh
@@ -80,7 +80,8 @@ execBrewBundle() {
   if [ "true" == "$CI" ]
   then
     brew update
-    export HOMEBREW_BUNDLE_MAS_SKIP=$(brew bundle list --mas | paste -sd " " -)
+    export HOMEBREW_BUNDLE_CASK_SKIP=$(brew bundle list --casks | awk '{$0=q $0 q; gsub(ORS,OFS)}1' q=\' RS= OFS="' '")
+    export HOMEBREW_BUNDLE_MAS_SKIP=$(brew bundle list --mas | awk '{$0=q $0 q; gsub(ORS,OFS)}1' q=\' RS= OFS="' '")
   fi
   brew bundle
 }
