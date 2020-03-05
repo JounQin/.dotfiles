8278147524bdfc50b6252cf251e7a0954746273f
JounQin <admin@1stg.me>
2020-03-05 14:28:32 +0800

    chore: format shellscripts with prettier

--- setup.sh
+++ setup.sh
@@ -8,12 +8,11 @@ command_exists() {
 
 setup_color() {
 	# Only use colors if connected to a terminal
-	if [ -t 1 ]
-  then
-    RED=$(printf '\033[31m')
-    GREEN=$(printf '\033[32m')
+	if [ -t 1 ]; then
+		RED=$(printf '\033[31m')
+		GREEN=$(printf '\033[32m')
 		YELLOW=$(printf '\033[33m')
-    BLUE=$(printf '\033[34m')
+		BLUE=$(printf '\033[34m')
 		RESET=$(printf '\033[m')
 	else
 		RED=""
@@ -29,80 +28,73 @@ setup_color
 echo "${RED}Homebrew, Zsh, Oh-My-Zsh, Zplugin and related apps and plugins will be installed automatically if not detected.${RESET}"
 
 # check Homebrew installation
-if ! command_exists brew
-then
-  echo "Installing Homebrew..."
-  if [[ "$OSTYPE" == "darwin"* ]]
-  then /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  else
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-    test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
-    test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-    echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
-    brew install zsh
-  fi
+if ! command_exists brew; then
+	echo "Installing Homebrew..."
+	if [[ "$OSTYPE" == "darwin"* ]]; then
+		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	else
+		sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+		test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+		test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+		test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+		echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+		brew install zsh
+	fi
 fi
 
 ## check Oh-My-Zsh installation
-if [ ! -d "$HOME/.oh-my-zsh" ]
-then
-  echo "Installing Oh-My-Zsh..."
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+	echo "Installing Oh-My-Zsh..."
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
 ## check Zplugin installation
-if [ ! -d "$HOME/.zplugin" ]
-then
-  echo "Installing Zplugin..."
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
+if [ ! -d "$HOME/.zplugin" ]; then
+	echo "Installing Zplugin..."
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
 fi
 
-if [ "true" == "$CI" ]
-then
-  echo "${YELLOW}CI detected, copy zshrc automatically.${RESET}"
-  \cp -f zshrc "$HOME/.zshrc"
+if [ "true" == "$CI" ]; then
+	echo "${YELLOW}CI detected, copy zshrc automatically.${RESET}"
+	\cp -f zshrc "$HOME/.zshrc"
 else
-  if read -t 5 -p "Copy or overwrite \`~/.zshrc\`? [y/N]: " -n 1 -r
-  then
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then \cp -f zshrc "$HOME/.zshrc"
-    fi
-  else
-    echo
-    echo "Exit automatically due to timeout..."
-    exit 0
-  fi
+	if read -t 5 -p "Copy or overwrite \`~/.zshrc\`? [y/N]: " -n 1 -r; then
+		if [[ $REPLY =~ ^[Yy]$ ]]; then
+			\cp -f zshrc "$HOME/.zshrc"
+		fi
+	else
+		echo
+		echo "Exit automatically due to timeout..."
+		exit 0
+	fi
 fi
 
 execBrewBundle() {
-  echo "Executing \`brew bundle\` to install packages and apps."
-  echo "${BLUE}It may take some time until finishing at the first time, please be a bit patience...${RESET}"
-  if [ "true" == "$CI" ]
-  then
-    export HOMEBREW_BUNDLE_TAP_SKIP="mathildetech/alauda"
-    if [[ "$OSTYPE" == "darwin"* ]]
-    then
-      export HOMEBREW_BUNDLE_BREW_SKIP="mathildetech/alauda/console-cli miderwong/flutter/flutter"
-      export HOMEBREW_BUNDLE_MAS_SKIP=$(grep "^mas.*id: \d*$" Brewfile | cut -d":" -f2 | paste -sd " " -)
-    else
-      export HOMEBREW_BUNDLE_BREW_SKIP="ideviceinstaller ios-deploy libimobiledevice mas mathildetech/alauda/console-cli"
-    fi
-  fi
-  brew update
-  brew bundle
+	echo "Executing \`brew bundle\` to install packages and apps."
+	echo "${BLUE}It may take some time until finishing at the first time, please be a bit patience...${RESET}"
+	if [ "true" == "$CI" ]; then
+		export HOMEBREW_BUNDLE_TAP_SKIP="mathildetech/alauda"
+		if [[ "$OSTYPE" == "darwin"* ]]; then
+			export HOMEBREW_BUNDLE_BREW_SKIP="mathildetech/alauda/console-cli miderwong/flutter/flutter"
+			export HOMEBREW_BUNDLE_MAS_SKIP=$(grep "^mas.*id: \d*$" Brewfile | cut -d":" -f2 | paste -sd " " -)
+		else
+			export HOMEBREW_BUNDLE_BREW_SKIP="ideviceinstaller ios-deploy libimobiledevice mas mathildetech/alauda/console-cli"
+		fi
+	fi
+	brew update
+	brew bundle
 }
 
 execBrewBundle || {
-  echo "${YELLOW}Error occurred, retrying.${RESET}"
-  execBrewBundle || {
-    echo "${RED}Retried but failed, exiting...${RESET}"
-    exit 1
-  }
+	echo "${YELLOW}Error occurred, retrying.${RESET}"
+	execBrewBundle || {
+		echo "${RED}Retried but failed, exiting...${RESET}"
+		exit 1
+	}
 }
 
 echo "Setting up rcm..."
-rcup  brew-aliases SwitchHosts gitconfig gitignore gitproxy zshrc
+rcup brew-aliases SwitchHosts gitconfig gitignore gitproxy zshrc
 
 echo "Symlinking VSCode workspaces..."
 WORKSPACE_SRC=~/.dotfiles/Workspaces
@@ -114,8 +106,7 @@ ln -sf $WORKSPACE_SRC/ufc.code-workspace $WORKSPACE_DEST
 
 echo -e "${GREEN}\xf0\x9f\x8e\x89 Congratulations, you have finished setting up!${RESET}"
 
-if [[ "true" != "$CI" && ! $SHELL =~ \/zsh$ ]]
-then
-  echo "${BLUE}Toggling to zsh.${RESET}"
-  exec zsh
+if [[ "true" != "$CI" && ! $SHELL =~ \/zsh$ ]]; then
+	echo "${BLUE}Toggling to zsh.${RESET}"
+	exec zsh
 fi
